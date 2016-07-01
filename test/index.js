c604c602a2648fd965ef810c99a328f4393b3ee2
Seth Trowbridge <seth111@gmail.com>
2016-07-01 10:33:12 -0400

    testing started

---
+++ test/index.js
@@ -0,0 +1,17 @@
+var chai = require('chai');
+var should = chai.should();
+
+describe("Mad Props social rewards site", function(){
+    it("should have a set of users", function(){
+        
+    });
+    it("should allow a user to write a story recognizing another user", function(){
+
+    });
+    it("should allow a user to vote on a story", function(){
+
+    });
+    it("should collect top stories and users within a time period", function(){
+
+    });
+});
