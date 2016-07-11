d315da56251ba0bcbd80ede4dc832544be0444d3
Seth Trowbridge <seth111@gmail.com>
2016-07-11 16:57:50 -0400

    can produce contest views

--- routes/contests.js
+++ routes/contests.js
@@ -1,13 +1,25 @@
 var router = require('express').Router();
 var db = require('../db/neo4j');
 
-router.get('/contest', function(inReq, inRes){
-    db.query("match (c:Contest)<-[:enter]-(s:Story) optional match (author:User)-[:wrote]->(s)-[:recognize]->(about:User) optional match (voter:User)-[v:vote]->(s) with c, author, s, about, count(v) as votes  return c, collect(author), collect(s), collect(about), collect(votes)")
+router.get('/contests', function(inReq, inRes){
+
+    var i, j;
+    var model = [];
+    var contest;
+    var story;
+    var users;
+
+    db.query("match (u:User) return u")
     .then(function(inSuccess){
-        var i, j;
-        var model = [];
-        var contest;
-        var story;
+
+        users = [];
+        for(i=0; i<inSuccess.length; i++){
+            users.push(inSuccess[i][0].data);
+        }
+
+        return db.query("match (c:Contest {open:true})<-[:enter]-(s:Story) optional match (author:User)-[:wrote]->(s)-[:recognize]->(about:User) optional match (voter:User)-[v:vote]->(s) with c, author, s, about, count(v) as votes  return c, collect(author), collect(s), collect(about), collect(votes)")
+    })
+    .then(function(inSuccess){
         for(i=0; i<inSuccess.length; i++){
             contest = {
                 contest:inSuccess[i][0].data,
@@ -18,15 +30,17 @@ router.get('/contest', function(inReq, inRes){
                     author:inSuccess[i][1][j].data,
                     story:inSuccess[i][2][j].data,
                     about:inSuccess[i][3][j].data,
-                    votes:inSuccess[i][4][j],
+                    votes:inSuccess[i][4][j]
                 });
             }
             model.push(contest);
         }
-        inRes.status(200).json(model);
+        inRes.render("contests", {layout:"main", id:inReq.Auth.ID, model:model, users:users});
     }, function(inFailure){
         inRes.status(500).json(inFailure);
     });
+
+    
 });
 
 module.exports = router;
