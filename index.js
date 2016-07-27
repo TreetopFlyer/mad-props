cc13507afc9525ef6e9f89abcccd71c4ffec1598
Seth Trowbridge <seth111@gmail.com>
2016-07-27 11:47:12 -0400

    added ability to use https in the config

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-require('./server.js').listen(80);
+var server = require('./server.js');
+var fs = require('fs');
+var https = require('https');
+var http = require('http');
+
+var port = process.env.PORT || 80;
+
+if(process.env.HTTPS === 'true')
+{
+    var options = {
+        key  : fs.readFileSync('ssl.key'),
+        cert : fs.readFileSync('ssl.crt')
+    };
+    https.createServer(options, server).listen(port, function (){
+        console.log('Started in HTTPS mode');
+    });
+}
+else
+{
+    http.createServer(server).listen(port, function (){
+        console.log('Started in plain HTTP mode');
+    });
+}
